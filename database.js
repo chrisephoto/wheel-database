61b8813897b3020f764ba50ee81f1ec65bc49fe4
Chris <chrisephotography@gmail.com>
2023-03-31 08:26:45 -0400

    Create database.js

---
+++ database.js
@@ -0,0 +1,89 @@
+dataset = [
+  {
+    brand: "Mugen",
+    model: "CF-48",
+    manufacturer: "Fortran",
+    images: ["https://kyushashoes.com/wp-content/uploads/2021/02/Mugen-CF-48-150x150.jpg","https://kyushashoes.com/wp-content/uploads/2021/01/Bel-Torino-Skyline-150x150.jpg","https://kyushashoes.com/wp-content/uploads/2021/01/Autostrada-MK8-black-150x150.jpg"],
+    sizes: [
+      {
+        diameter: "15",
+        width: "6.5",
+        pcd: "5x114.3",
+        offsets: "+38",
+        colors: "Silver, Bronze"
+      },
+      {
+        diameter: "15",
+        width: "7",
+        pcd: "5x114.3",
+        offsets: "+40",
+        colors: "Silver, Bronze"
+      },
+      {
+        diameter: "16",
+        width: "7",
+        pcd: "5x114.3",
+        offsets: "+43/+53",
+        colors: "Silver, Bronze"
+      }
+    ]
+  },
+  {
+    brand: "Bel Torino",
+    model: "Skyline",
+    manufacturer: "Rays Engineering",
+    images: ["https://kyushashoes.com/wp-content/uploads/2021/01/Bel-Torino-Skyline-150x150.jpg","https://kyushashoes.com/wp-content/uploads/2021/01/Autostrada-MK8-black-150x150.jpg","https://kyushashoes.com/wp-content/uploads/2021/02/Mugen-CF-48-150x150.jpg"],
+    sizes: [
+      {
+        diameter: "15",
+        width: "6.5",
+        pcd: "5x114.3",
+        offsets: "+38",
+        colors: "Silver, Bronze"
+      },
+      {
+        diameter: "15",
+        width: "7",
+        pcd: "5x114.3",
+        offsets: "+40",
+        colors: "Silver, Bronze"
+      },
+      {
+        diameter: "16",
+        width: "7",
+        pcd: "5x114.3",
+        offsets: "+43/+53",
+        colors: "Silver, Bronze"
+      }
+    ]
+  },
+  {
+    brand: "Autostrada",
+    model: "MK8",
+    manufacturer: "Rays Engineering",
+    images: ["https://kyushashoes.com/wp-content/uploads/2021/01/Autostrada-MK8-black-150x150.jpg","https://kyushashoes.com/wp-content/uploads/2021/02/Mugen-CF-48-150x150.jpg","https://kyushashoes.com/wp-content/uploads/2021/01/Bel-Torino-Skyline-150x150.jpg"],
+    sizes: [
+      {
+        diameter: "15",
+        width: "6.5",
+        pcd: "5x114.3",
+        offsets: "+38",
+        colors: "Silver, Bronze"
+      },
+      {
+        diameter: "15",
+        width: "7",
+        pcd: "5x114.3",
+        offsets: "+40",
+        colors: "Silver, Bronze"
+      },
+      {
+        diameter: "16",
+        width: "7",
+        pcd: "5x114.3",
+        offsets: "+43/+53",
+        colors: "Silver, Bronze"
+      }
+    ]
+  }
+];
