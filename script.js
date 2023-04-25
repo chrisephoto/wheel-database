b9d9e86d65408f6192983239587b592805a1472b
Chris <chrisephotography@gmail.com>
2023-04-25 10:49:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -125,15 +125,15 @@ function applyFilter() {
     matchSize = true;
     matchPCD = true;
     
-    if (filterBrand && dataset[i].brand != filterBrand) {
+    if (filterBrand != "All" && dataset[i].brand != filterBrand) {
       matchBrand = false;
     }
     for (let j = 0; j < filterYear.length; j++) {
-      if (filterYear[j] >= dataset[i].yearStart && filterYear[j] <= dataset[i].yearEnd) {
+      if ((filterYear[j] >= dataset[i].yearStart && filterYear[j] <= dataset[i].yearEnd) || filterYear[j] == "All") {
         matchYear = true;
       }
     }
-    if (filterStyle && dataset[i].style != filterStyle) {
+    if (filterStyle != "All" && dataset[i].style != filterStyle) {
       matchStyle = false;
     }
     if (matchBrand && matchYear && matchStyle) {
