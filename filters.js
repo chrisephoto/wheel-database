c3cb1be42a833bc8a3579eb6c440ccdb30e291e9
Chris <chrisephotography@gmail.com>
2023-04-25 12:09:33 -0400

    Update filters.js

--- filters.js
+++ filters.js
@@ -1,32 +1,30 @@
-filters = [
-  {
-    brand: [
-      "BeeR",
-      "Uras",
-      "Ings"
-      ],
-    manufacturer: [
-      "Unverified",
-      "Rays Co., Ltd."
-      ],
-    style: [
-      "5 Spoke",
-      "6 Spoke"
-      ],
-    size: [
-      "17",
-      "18"
-      ],
-    pcd: [
-      "5x100",
-      "5x114.3"
-      ],
-    year: [
-      "2004",
-      "2005",
-      "2006",
-      "2007",
-      "2008"
-      ]
-  }
-];
+filters =  {
+  brand: [
+    "BeeR",
+    "Uras",
+    "Ings"
+    ],
+  manufacturer: [
+    "Unverified",
+    "Rays Co., Ltd."
+    ],
+  style: [
+    "5 Spoke",
+    "6 Spoke"
+    ],
+  size: [
+    "17",
+    "18"
+    ],
+  pcd: [
+    "5x100",
+    "5x114.3"
+    ],
+  year: [
+    "2004",
+    "2005",
+    "2006",
+    "2007",
+    "2008"
+    ]
+}
