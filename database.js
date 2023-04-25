91a99e09a66b96c372e45e81f7cb22fa01b7884e
Chris <chrisephotography@gmail.com>
2023-04-25 18:24:10 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,123 @@
 dataset = [
   {
+    id: "0YDS9EBJLVAB",
+    shortname: "Bee&#8902;R B5",
+    description: "Dual valvestem. Nismo LMGT4 style. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra non risus in tempus. In vulputate suscipit nunc, et pharetra neque condimentum in. Vivamus id dapibus orci. Mauris accumsan dignissim nulla, quis semper nibh porta nec. Nullam eu metus sit amet ligula consequat pulvinar. Etiam vitae porta erat. Morbi sed posuere arcu. Aliquam a nisi id ligula facilisis consectetur a et tortor. Pellentesque mollis rhoncus nisl, et commodo nisl elementum eget.",
+    brand: "Bee&#8902;R",
+    model: "B5",
+    manufacturer: "Unverified",
+    yearStart: "Unverified",
+    yearEnd: "Unverified",
+    origin: "Unverified",
+    construction: "1 Piece - Unverified",
+    style: "5 Spoke",
+    link: "http://web.archive.org/web/20100116090507/http://www.bee-r.com/main/parts/wheel/index.htm",
+    images: ["http://web.archive.org/web/20100116090507im_/http://www.bee-r.com/main/parts/wheel/photo/wheelgold01.jpg"],
+    sizes: [
+      {
+        diameter: "17",
+        width: "9.0",
+        pcd: "5x114.3",
+        offsets: "+25",
+        colors: "White, Silver, Bronze, Chrome"
+      },
+      {
+        diameter: "18",
+        width: "9.5",
+        pcd: "5x114.3",
+        offsets: "+25/+38",
+        colors: "White, Silver, Bronze, Chrome"
+      }
+    ]
+  },
+  {
+    id: "T8HREQGJ1TRN",
+    shortname: "Ings TS06",
+    description: "Manufactured by Rays.",
+    brand: "Ings",
+    model: "TS06",
+    manufacturer: "Rays Co., Ltd.",
+    yearStart: "2007",
+    yearEnd: "2007",
+    origin: "Japan",
+    construction: "1 Piece - Forged",
+    style: "Split 6 Spoke",
+    link: "https://www.ings-net.com/english/products/wheel.html",
+    images: ["images/T8HREQGJ1TRN-01.jpg","images/T8HREQGJ1TRN-02.jpg","images/T8HREQGJ1TRN-03.jpg","images/T8HREQGJ1TRN-04.jpg","images/T8HREQGJ1TRN-05.jpg"],
+    sizes: [
+      {
+        diameter: "18",
+        width: "7.5",
+        pcd: "5x100",
+        offsets: "+43/+48",
+        colors: "Silver, Gunmetal, Gold"
+      },
+      {
+        diameter: "18",
+        width: "7.5",
+        pcd: "5x114.3",
+        offsets: "+43/+48",
+        colors: "Silver, Gunmetal, Gold"
+      },
+      {
+        diameter: "18",
+        width: "8.0",
+        pcd: "5x100",
+        offsets: "+38/+43",
+        colors: "Silver, Gunmetal, Gold"
+      },
+      {
+        diameter: "18",
+        width: "8.0",
+        pcd: "5x114.3",
+        offsets: "+38/+43",
+        colors: "Silver, Gunmetal, Gold"
+      },
+      {
+        diameter: "18",
+        width: "8.5",
+        pcd: "5x100",
+        offsets: "+43",
+        colors: "Silver, Gunmetal, Gold"
+      },
+      {
+        diameter: "18",
+        width: "8.5",
+        pcd: "5x114.3",
+        offsets: "+22/+30/+38",
+        colors: "Silver, Gunmetal"
+      },
+      {
+        diameter: "18",
+        width: "8.5",
+        pcd: "5x114.3",
+        offsets: "+43",
+        colors: "Silver, Gunmetal, Gold"
+      },
+      {
+        diameter: "18",
+        width: "9.0",
+        pcd: "5x114.3",
+        offsets: "+35/+50",
+        colors: "Silver, Gunmetal"
+      },
+      {
+        diameter: "18",
+        width: "9.5",
+        pcd: "5x114.3",
+        offsets: "+12/+22/+34/+40",
+        colors: "Silver, Gunmetal"
+      },
+      {
+        diameter: "18",
+        width: "10.5",
+        pcd: "5x114.3",
+        offsets: "+15/+22",
+        colors: "Silver, Gunmetal"
+      }
+    ]
+  },
+  {
     id: "XUN1UOX4TSJA",
     shortname: "Southern Ways S-05",
     description: "Dual valvestem. Nismo LMGT4 style. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra non risus in tempus. In vulputate suscipit nunc, et pharetra neque condimentum in. Vivamus id dapibus orci. Mauris accumsan dignissim nulla, quis semper nibh porta nec. Nullam eu metus sit amet ligula consequat pulvinar. Etiam vitae porta erat. Morbi sed posuere arcu. Aliquam a nisi id ligula facilisis consectetur a et tortor. Pellentesque mollis rhoncus nisl, et commodo nisl elementum eget.",
@@ -129,37 +247,6 @@ dataset = [
     ]
   },
   {
-    id: "0YDS9EBJLVAB",
-    shortname: "Bee&#8902;R B5",
-    description: "Dual valvestem. Nismo LMGT4 style. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra non risus in tempus. In vulputate suscipit nunc, et pharetra neque condimentum in. Vivamus id dapibus orci. Mauris accumsan dignissim nulla, quis semper nibh porta nec. Nullam eu metus sit amet ligula consequat pulvinar. Etiam vitae porta erat. Morbi sed posuere arcu. Aliquam a nisi id ligula facilisis consectetur a et tortor. Pellentesque mollis rhoncus nisl, et commodo nisl elementum eget.",
-    brand: "Bee&#8902;R",
-    model: "B5",
-    manufacturer: "Unverified",
-    yearStart: "Unverified",
-    yearEnd: "Unverified",
-    origin: "Unverified",
-    construction: "1 Piece - Unverified",
-    style: "5 Spoke",
-    link: "http://web.archive.org/web/20100116090507/http://www.bee-r.com/main/parts/wheel/index.htm",
-    images: ["http://web.archive.org/web/20100116090507im_/http://www.bee-r.com/main/parts/wheel/photo/wheelgold01.jpg"],
-    sizes: [
-      {
-        diameter: "17",
-        width: "9.0",
-        pcd: "5x114.3",
-        offsets: "+25",
-        colors: "White, Silver, Bronze, Chrome"
-      },
-      {
-        diameter: "18",
-        width: "9.5",
-        pcd: "5x114.3",
-        offsets: "+25/+38",
-        colors: "White, Silver, Bronze, Chrome"
-      }
-    ]
-  },
-  {
     id: "IPL5GT7KH9V1",
     shortname: "Uras NS-01",
     description: "Rebadged DoAll/CST D Spec. Possibly manufactured by Weds.",
@@ -225,93 +312,6 @@ dataset = [
         pcd: "5x114.3",
         offsets: "+15",
         colors: "Chrome"
-      }
-    ]
-  },
-  {
-    id: "T8HREQGJ1TRN",
-    shortname: "Ings TS06",
-    description: "Manufactured by Rays.",
-    brand: "Ings",
-    model: "TS06",
-    manufacturer: "Rays Co., Ltd.",
-    yearStart: "2007",
-    yearEnd: "2007",
-    origin: "Japan",
-    construction: "1 Piece - Forged",
-    style: "Split 6 Spoke",
-    link: "https://www.ings-net.com/english/products/wheel.html",
-    images: ["images/T8HREQGJ1TRN-01.jpg"],
-    sizes: [
-      {
-        diameter: "18",
-        width: "7.5",
-        pcd: "5x100",
-        offsets: "+43/+48",
-        colors: "Silver, Gunmetal, Gold"
-      },
-      {
-        diameter: "18",
-        width: "7.5",
-        pcd: "5x114.3",
-        offsets: "+43/+48",
-        colors: "Silver, Gunmetal, Gold"
-      },
-      {
-        diameter: "18",
-        width: "8.0",
-        pcd: "5x100",
-        offsets: "+38/+43",
-        colors: "Silver, Gunmetal, Gold"
-      },
-      {
-        diameter: "18",
-        width: "8.0",
-        pcd: "5x114.3",
-        offsets: "+38/+43",
-        colors: "Silver, Gunmetal, Gold"
-      },
-      {
-        diameter: "18",
-        width: "8.5",
-        pcd: "5x100",
-        offsets: "+43",
-        colors: "Silver, Gunmetal, Gold"
-      },
-      {
-        diameter: "18",
-        width: "8.5",
-        pcd: "5x114.3",
-        offsets: "+22/+30/+38",
-        colors: "Silver, Gunmetal"
-      },
-      {
-        diameter: "18",
-        width: "8.5",
-        pcd: "5x114.3",
-        offsets: "+43",
-        colors: "Silver, Gunmetal, Gold"
-      },
-      {
-        diameter: "18",
-        width: "9.0",
-        pcd: "5x114.3",
-        offsets: "+35/+50",
-        colors: "Silver, Gunmetal"
-      },
-      {
-        diameter: "18",
-        width: "9.5",
-        pcd: "5x114.3",
-        offsets: "+12/+22/+34/+40",
-        colors: "Silver, Gunmetal"
-      },
-      {
-        diameter: "18",
-        width: "10.5",
-        pcd: "5x114.3",
-        offsets: "+15/+22",
-        colors: "Silver, Gunmetal"
       }
     ]
   }
