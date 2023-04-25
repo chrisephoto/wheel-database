e0aac0c909e433c53b9c87de9e4cc535b41d681d
Chris <chrisephotography@gmail.com>
2023-04-25 12:02:46 -0400

    Create filters.js

---
+++ filters.js
@@ -0,0 +1,34 @@
+filters = [
+  {
+    brand: [
+      "All",
+      "BeeR",
+      "Uras",
+      "Ings"
+      ],
+    manufacturer: [
+      "All",
+      "Unverified",
+      "Rays Co., Ltd."
+      ],
+    style: [
+      "5 Spoke",
+      "6 Spoke"
+      ],
+    size: [
+      "17",
+      "18"
+      ],
+    pcd: [
+      "5x100",
+      "5x114.3"
+      ],
+    year: [
+      "2004",
+      "2005",
+      "2006",
+      "2007",
+      "2008"
+      ]
+  }
+];
